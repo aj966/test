e5c6f5011e2eed295d8375efd99e2fa478a76a72
aj966 <47604595+aj966@users.noreply.github.com>
2019-02-16 15:43:41 +0300

    Update 9.php

--- 9.php
+++ 9.php
@@ -2,15 +2,23 @@
 
 	require_once 'QiwiApi.php';
 	
-	if (!isset($_POST['phone'], $_POST['token'], $_POST['type'])) {
+	if (!isset($_POST['phone'], $_POST['token'], $_POST['link'], $_POST['params'])) {
 		exit('error params');
 	}
 	$phone = $_POST['phone'];
 	$token = $_POST['token'];
-	$type = $_POST['type'];
+	$link  = urldecode($_POST['link']);
+	$params = json_decode(urldecode($_POST['params']));
 
 	$api = new QiwiApi($phone, $token);
    
+	echo json_encode([
+		'success' => 1, 
+		'response' => $api->call($link, $params)
+	], JSON_NUMERIC_CHECK);
+	exit();
+
+
 	switch ($type) {
 		case 'getBalance':
 			$a = $api->getBalance($phone);
